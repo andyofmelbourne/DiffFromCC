6042b2018873be4335487033ab3d61855d11b933
andyofmelbourne <andyofmelbourne@gmail.com>
2014-07-21 13:45:45 +0200

    Add header

--- crossTermsTools.py
+++ crossTermsTools.py
@@ -1,5 +1,10 @@
-
-#########################################################
+#------------------------------------------------------------------------------------------
+# Authors: Andrew Mortin, Andrew Morgan
+# Paper reference: 
+#   A. Martin, A. Morgan, T. Ekeberg, N. Loh, F. Maia, F. Wang, J. Spence, and H. Chapman, 
+#   "The extraction of single-particle diffraction patterns from a multiple-particle 
+#   diffraction pattern," Opt. Express  21, 15102-15112 (2013).
+#
 # Based on CrossTermsMod.py by A. Morgan 09/2011
 # 
 # A. Martin 10/2011 
@@ -7,7 +12,7 @@
 #       - only necessary functions retained
 #   - cross.project2Fast renamed cross.project()
 #       - many functions moved to MorgansFunctions.py
-#########################################################
+#------------------------------------------------------------------------------------------
 
 
 import numpy as np
